6e33ad5ed3cd2d9b49785771cf3b181d496e515d
SeptAlfauzan <alfauzansepta@gmail.com>
2020-04-22 11:49:49 +0700

    Updates

---
+++ precache-manifest.cee677c5dcb7515d1b0c975450afbb36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d65990f49c35cfc930c5853476a6e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b2bfd7301ae9bf4e89",
+    "url": "/static/css/2.5ffc8bb3.chunk.css"
+  },
+  {
+    "revision": "63409b6464c79ad953e6",
+    "url": "/static/css/main.58a20946.chunk.css"
+  },
+  {
+    "revision": "60b2bfd7301ae9bf4e89",
+    "url": "/static/js/2.e72fdddb.chunk.js"
+  },
+  {
+    "revision": "44ca7754162f09506dec3643ba30b719",
+    "url": "/static/js/2.e72fdddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63409b6464c79ad953e6",
+    "url": "/static/js/main.71f2d29f.chunk.js"
+  },
+  {
+    "revision": "9dd3d90ece54fe922842",
+    "url": "/static/js/runtime-main.4bbfb4d4.js"
+  },
+  {
+    "revision": "8fa9e808e1ff57c6859e7a3aebaa2a0a",
+    "url": "/static/media/Location.8fa9e808.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-tracker/precache-manifest.02e7203d8bf80193aa8ce6ecf1f763bd.js"
+  "/precache-manifest.cee677c5dcb7515d1b0c975450afbb36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
